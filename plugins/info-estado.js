65501914077f872b8978fdd2a25ff2b99913aaed
Yixzn <115938737+Yixzn@users.noreply.github.com>
2022-10-22 20:44:17 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -8,7 +8,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
 let estado =`
-╭─[ *𝐍𝐨𝐭𝐉𝐡𝐮𝐳ᙆ  ᷦ - MD* ]
+╭─[ *@𝕘𝕚𝕠𝕠𝕫𝕟_ᙆ  ᷦ - MD* ]
 │ *➤ 𝙷𝙾𝙻𝙰 ${name}*
 │
 │ *ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
